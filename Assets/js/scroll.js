70c5af9ce77bc2580b117fb8461c2f53c56fdd25
kristelgrace <60626354+kristelgrace@users.noreply.github.com>
2020-05-11 21:41:13 -0400

    Uploaded website

---
+++ Assets/js/scroll.js
@@ -0,0 +1,22 @@
+// window.sr = ScrollReveal({
+
+
+var config = {
+  origin: 'bottom',
+  distance: '40px',
+  duration: 500,
+  easing: 'ease',
+  mobile: true,
+  reset: false,
+  viewFactor: 0.4
+};
+
+window.sr = ScrollReveal(config)
+
+sr.reveal('#bunbun-work', {delay: 100});
+
+sr.reveal('#junto-work', {delay: 300});
+
+sr.reveal('#comyoot-work', {delay: 100});
+
+sr.reveal('#pubart-work', {delay: 300});
