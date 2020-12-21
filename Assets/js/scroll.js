7554a76c1768c114187b869deb7a51701e283977
kristelgrace <60626354+kristelgrace@users.noreply.github.com>
2020-12-21 13:58:46 +0800

    Added scroll reveal for junto affinity diagram

--- Assets/js/scroll.js
+++ Assets/js/scroll.js
@@ -1,6 +1,3 @@
-// window.sr = ScrollReveal({
-
-
 var config = {
   origin: 'bottom',
   distance: '40px',
@@ -14,9 +11,25 @@ var config = {
 window.sr = ScrollReveal(config)
 
 sr.reveal('#bunbun-work', {delay: 100});
-
 sr.reveal('#junto-work', {delay: 300});
-
 sr.reveal('#comyoot-work', {delay: 100});
-
 sr.reveal('#creart-work', {delay: 300});
+
+// reveal text annotations on project images
+sr.reveal('#junto-affinity-text1', {delay: 200});
+sr.reveal('#junto-affinity-text2', {delay: 1800});
+
+// reveal the full overlay
+var configa = {
+  origin: 'bottom',
+  distance: '40px',
+  duration: 0,
+  easing: 'ease',
+  mobile: true,
+  reset: false,
+  viewFactor: 0.4
+};
+
+window.sra = ScrollReveal(configa)
+
+sra.reveal('#junto-affinity-complete', {delay: 2200});
