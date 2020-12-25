14b60d4de4f96e50120e585b3f1d3d64bf324cf2
kristelgrace <60626354+kristelgrace@users.noreply.github.com>
2020-12-25 21:48:04 +0800

    Creart image shadows and fixed Junto video autoplay issue

---
+++ Assets/js/autoplay.js
@@ -0,0 +1,28 @@
+// adjust autoplay trigger on scroll, sourced from stackedoverflow
+// https://stackoverflow.com/questions/15395920/play-html5-video-when-scrolled-to
+
+$(document).ready(function() {
+// Get media - with autoplay disabled (audio or video)
+var media = $('video').not("[autoplay='autoplay']");
+var tolerancePixel = 550;
+
+function checkMedia(){
+    // Get current browser top and bottom
+    var scrollTop = $(window).scrollTop() + tolerancePixel;
+    var scrollBottom = $(window).scrollTop() + $(window).height() - tolerancePixel;
+
+    media.each(function(index, el) {
+        var yTopMedia = $(this).offset().top;
+        var yBottomMedia = $(this).height() + yTopMedia;
+
+        if(scrollTop < yBottomMedia && scrollBottom > yTopMedia){ //view explaination in `In brief` section above
+            $(this).get(0).play();
+        } else {
+            $(this).get(0).pause();
+        }
+    });
+
+    //}
+}
+$(document).on('scroll', checkMedia);
+});
