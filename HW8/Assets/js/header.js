d571f194dfe168f9aeff7d9738cff615a96607fb
kristelgrace <60626354+kristelgrace@users.noreply.github.com>
2020-12-26 21:13:55 +0800

    Deleted files not in use

--- HW8/Assets/js/header.js
+++
@@ -1,6 +0,0 @@
-// grab an element
-var myElement = document.querySelector(".headroom");
-// construct an instance of Headroom, passing the element
-var headroom  = new Headroom(myElement);
-// initialise
-headroom.init();
